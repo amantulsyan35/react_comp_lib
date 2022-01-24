35bccc760e2f126d7d24a1154918fedaf2527553
Aman Tulsyan <65289994+amantulsyan35@users.noreply.github.com>
2022-01-24 19:28:08 +0530

    react_comp_lib

---
+++ src/components/Avatar/Avatar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Avatar.css';
+
+const Avatar = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default Avatar;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './index.css';
+import Avatar from './components/Avatar/Avatar';
+
+export { Avatar };
